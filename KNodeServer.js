a74092f40a67a6bfb29eaf5752b7f2b7181a5274
Ken Ceglia <kpznet@outlook.com>
2018-12-21 07:49:40 -0800

    Update

--- KNodeServer.js
+++ KNodeServer.js
@@ -169,7 +169,7 @@ function post_initialize() {
     }
     construct_my_address_space(server);
     server.start(function() {
-        console.log("Server is now listening ... ( press CTRL+C to stop)");
+        console.log("KUAServer is now listening ... ( press CTRL+C to stop)");
         console.log("port ", server.endpoints[0].port);
         const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
         console.log(" the primary server endpoint url is ", endpointUrl );
