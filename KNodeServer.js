bd7d9d5b7051a81d9c8e1136bf04297b6cd45800
Ken Ceglia <kpznet@outlook.com>
2018-12-22 08:31:20 -0800

    Nodes

--- KNodeServer.js
+++ KNodeServer.js
@@ -3,14 +3,16 @@ const opcua = require("node-opcua");
 
 // Let's create an instance of OPCUAServer
 const server = new opcua.OPCUAServer({
-    port: 4334, // the port of the listening socket of the server
-    resourcePath: "UA/MaxKServer", // this path will be added to the endpoint resource name
+    port: 4336, // the port of the listening socket of the server
+    resourcePath: "UA/KServer", // this path will be added to the endpoint resource name
      buildInfo : {
-        productName: "MaxKServer",
-        buildNumber: "7658",
-        buildDate: new Date(2014,5,2)
+        productName: "KServer",
+        buildNumber: "8000",
+        buildDate: new Date(2018,12,10)
     }
 });
+
+let totalNodes = 0
 
 function AddMeNode(parenter, oName, numVarSets, vUpdaterString, vUpdaterDouble, vUpdaterInt){
 
@@ -23,7 +25,7 @@ function AddMeNode(parenter, oName, numVarSets, vUpdaterString, vUpdaterDouble, 
         browseName: oName
     });
 
-
+    totalNodes += (numVarSets * 3);
     let i = 0
     for( i = 0; i< numVarSets;i++)
     {
@@ -64,7 +66,7 @@ function AddMeNode(parenter, oName, numVarSets, vUpdaterString, vUpdaterDouble, 
 }
 
 
-function AddTree2(parentNode, baseName, numNodes, maxDepth, treeDepth)
+function AddTree2(parentNode, baseName, numNodes, maxDepth, numVars, treeDepth)
 {
     // add some variables 
     // add a variable named MyVariable1 to the newly created folder "MyDevice"
@@ -76,16 +78,19 @@ function AddTree2(parentNode, baseName, numNodes, maxDepth, treeDepth)
     if( treeDepth <= maxDepth  )
     {
         let i = 1
+
+        totalNodes += treeDepth;
+
         for(i = 1; i <= numNodes; i++)
         {
             if(treeDepth == maxDepth)
             {
-                iP = AddMeNode(parentNode, baseName + " " + treeDepth +  " " + i, 4, vUpdatedString, vUpdatedDouble, vUpdatedInt);
+                iP = AddMeNode(parentNode, baseName + " " + treeDepth +  " " + i, numVars, vUpdatedString, vUpdatedDouble, vUpdatedInt);
             }
             else
             {
-                iP = AddMeNode(parentNode, baseName + " " + treeDepth +  " " + i, 4, vUpdatedString, vUpdatedDouble, vUpdatedInt);
-                AddTree2(iP, baseName, numNodes, maxDepth, treeDepth);
+                iP = AddMeNode(parentNode, baseName + " " + treeDepth +  " " + i, numVars, vUpdatedString, vUpdatedDouble, vUpdatedInt);
+                AddTree2(iP, baseName, numNodes, maxDepth, numVars, treeDepth);
             }
         }
 
@@ -103,30 +108,65 @@ function post_initialize() {
         // declare a new object
         const device = namespace.addObject({
             organizedBy: addressSpace.rootFolder.objects,
-            browseName: "MyDevice"
+            browseName: "GNoot"
         });
 
+        const numNodes = 3
+        const treeDepth = 3
+        const numVars = 1
 
-        AddTree2(device, "KObj", 5, 5, 0);
-
-        // add some variables 
-        // add a variable named MyVariable1 to the newly created folder "MyDevice"
-        let variable1 = 1;
         
-        // emulate variable1 changing every 500 ms
-        setInterval(function(){  variable1+=1; }, 500);
+
+        AddTree2(device, "KNode", numNodes, treeDepth, numVars, 0);
+
+
+        
+        // emulate x changing every 500 ms
+        //setInterval(function(){  variable1+=1; }, 500);
         
         namespace.addVariable({
             componentOf: device,
-            browseName: "MV1",
-            dataType: "Double",
+            browseName: "Number of Nodes per Branch",
+            dataType: "Int32",
             value: {
                 get: function () {
-                    return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1 });
+                    return new opcua.Variant({dataType: opcua.DataType.Int32, value: numNodes });
                 }
             }
         });
+        namespace.addVariable({
+            componentOf: device,
+            browseName: "Number of Depth",
+            dataType: "Int32",
+            value: {
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.Int32, value: treeDepth });
+                }
+            }
+        });        
+        namespace.addVariable({
+            componentOf: device,
+            browseName: "Number of Variables",
+            dataType: "Int32",
+            value: {
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.Int32, value: numVars });
+                }
+            }
+        });
+        namespace.addVariable({
+            componentOf: device,
+            browseName: "Total Nodes",
+            dataType: "Int32",
+            value: {
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.Int32, value: totalNodes });
+                }
+            }
+        });
+
         
+        /*
         // add a variable named MyVariable2 to the newly created folder "MyDevice"
         let variable2 = 10.0;
         
@@ -145,6 +185,7 @@ function post_initialize() {
                 }
             }
         });
+        */
         const os = require("os");
         /**
          * returns the percentage of free memory on the running machine
@@ -167,6 +208,7 @@ function post_initialize() {
             }
         });
     }
+    console.log("KUAServer initializing...");
     construct_my_address_space(server);
     server.start(function() {
         console.log("KUAServer is now listening ... ( press CTRL+C to stop)");
